08c09ffcd90f8f210f355e39a1a021442bf4e812
Luis Alvarez <lealvarezp@unac.edu.co>
2022-09-20 12:42:30 -0500

    sesión 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const defaultCounter = 0;
+  const [counter, setCounter] = useState(defaultCounter);
+  const decrease = () => setCounter(counter - 1);
+  const reset = () => setCounter(defaultCounter);
+  const increment = () => setCounter(counter + 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Counter</h1>
+      <h3>Current value: {counter} </h3>
+      <div className='actions'>
+        <button onClick={decrease} className='actions__button'>
+          -1
+        </button>
+        <button onClick={reset} className='actions__button'>
+          Reset
+        </button>
+        <button onClick={increment} className='actions__button'>
+          +1
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
