3123509d2afd9c78032c676f05a21f5d945c7c3b
Luis Alvarez <lealvarezp@unac.edu.co>
2022-09-27 13:29:37 -0500

    sesion 2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Counter } from './components/Counter';
+
+const App = () => {
+  return (
+    <>
+      <Counter defaultCounter={5} />
+    </>
+  );
+};
+
+export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Counter.css';
 
-const App = () => {
-  const defaultCounter = 0;
+export const Counter = (props) => {
+  const { defaultCounter } = props;
   const [counter, setCounter] = useState(defaultCounter);
   const decrease = () => setCounter(counter - 1);
   const reset = () => setCounter(defaultCounter);
   const increment = () => setCounter(counter + 1);
+
   return (
     <div className='app'>
       <h1>Counter</h1>
@@ -26,4 +28,6 @@ const App = () => {
   );
 };
 
-export default App;
+Counter.propTypes = {
+  defaultCounter: PropTypes.number.isRequired,
+};
